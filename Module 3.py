8ada5d29a269c224436106367e5fb7b8fe2a2eef
Roberto Roman <91975410+rroman5@users.noreply.github.com>
2021-10-17 18:42:16 -0500

    Module 3 Python code
    In this file, we have developed a python assignment where we created a program that execute tasks.

---
+++ Module 3.py
@@ -0,0 +1,55 @@
+#Roberto Roman
+#10-15-2021
+
+#Problem 1
+#This Programm will display Hello World to the screen.
+print ("Hello World")
+
+#Problem 2
+#This programm will display a message asking the user for its name to greet them
+
+name = input ("Please enter your name: ")
+print ("Hello", name)
+
+#Problem 3
+#This program will display a interactive mini conversation.
+
+name2 = input ("Name of the proffesor: ")
+print ("Hello", name2)
+
+
+#Problem 4
+#With this programm we will try to calculate the area of a circle using the given number of radius
+
+pi = 3.14
+r = float (input(" What is your Radius: "))
+
+mean = str( pi + r**2)
+
+print ("Your area of circle with radius is",mean)
+
+#Problem 5
+#With this program we will compute the MPG of a trip using a calculation formula.
+#
+
+miles = float (input ("Please enter the number of miles driven"))
+gallons = float (input ("Please enter the number of gallons used"))
+
+mpg = miles / gallons
+
+print ("Your MPG in a car is", mpg)
+
+#Problem 6
+#In this programm we will convert the given Fahrenheit to degrees Celsius
+
+fahrenheit = float (input("Enter temperature in fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print("Your given fahrenheit in celsius is", celsius)
+
+
+
+
+
+
+ 
+
